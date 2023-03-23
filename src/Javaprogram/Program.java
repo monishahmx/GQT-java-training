21e7c052439e6956ab64f273ca74b12ca0e085b6
Monisha <Monisha@DESKTOP-IRH1S1V>
2023-03-23 18:39:26 +0530

    commit

---
+++ src/Javaprogram/Program.java
@@ -0,0 +1,12 @@
+package Javaprogram;
+
+public class Program {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
