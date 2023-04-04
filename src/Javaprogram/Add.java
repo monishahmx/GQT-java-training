959bbc75f5664ef8692cd91ed92d5e9477924ef3
Monisha <Monisha@DESKTOP-IRH1S1V>
2023-04-04 17:32:14 +0530

    commit

---
+++ src/Javaprogram/Add.java
@@ -0,0 +1,17 @@
+package Javaprogram;
+import java.util.Scanner;
+
+public class Add {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the teo numbers:");
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = a+b;
+		System.out.println("Addition result is:"+c);
+		// TODO Auto-generated method stub
+
+	}
+
+}
