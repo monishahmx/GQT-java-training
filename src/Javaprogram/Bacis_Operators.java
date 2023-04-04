382ee7e5b7c8da4fe04dd3fa92ce6b64d707f2fc
Monisha <Monisha@DESKTOP-IRH1S1V>
2023-04-04 19:52:54 +0530

    commit

---
+++ src/Javaprogram/Bacis_Operators.java
@@ -0,0 +1,25 @@
+package Javaprogram;
+import java.util.Scanner;
+
+public class Bacis_Operators {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the two numbers:");
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = a+b;
+		System.out.println("Addition result is:"+c);
+		int d =  a-b;
+		System.out.println("Subtraction result is:"+d);
+		int e = a*b;
+		System.out.println("Multiplication result is:"+e);
+		int f = a/b;
+		System.out.println("Division result:"+f);
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
