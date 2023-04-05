4760c17b5066d220ed07b2c4c1285a961471540c
Monisha <Monisha@DESKTOP-IRH1S1V>
2023-04-05 16:00:31 +0530

    commit

---
+++ src/Javaprogram/First_Program.java
@@ -0,0 +1,11 @@
+package Javaprogram;
+
+public class First_Program {
+
+	public static void main(String[] args) {
+		System.out.println("HELLO WORLD");
+		// TODO Auto-generated method stub
+
+	}
+
+}
