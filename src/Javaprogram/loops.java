e6181eb6a44006adc4c67a9f48d5076f3ced215a
Monisha <Monisha@DESKTOP-IRH1S1V>
2023-04-06 16:39:53 +0530

    commit

---
+++ src/Javaprogram/loops.java
@@ -0,0 +1,13 @@
+package Javaprogram;
+
+public class loops {
+
+	public static void main(String[] args) {
+		for(int i=0;i<=6;i++) {
+			System.out.println("#");
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
