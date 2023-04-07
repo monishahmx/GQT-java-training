c82d04a921ac34c8b0e7ba9a36bca371aaf746f8
Monisha <Monisha@DESKTOP-IRH1S1V>
2023-04-07 23:32:08 +0530

    commit

---
+++ src/com/gqt/stringsproj/package1/Stringcode1.java
@@ -0,0 +1,13 @@
+//literal Method
+package com.gqt.stringsproj.package1;
+
+public class Stringcode1 {
+
+	public static void main(String[] args) {
+		String s1 ="Monisha";
+		System.out.println(s1);
+		
+		// TODO Auto-generated method stub
+
+	}
+}
---
+++ src/com/gqt/stringsproj/package1/Stringcode2.java
@@ -0,0 +1,13 @@
+//Object Method
+package com.gqt.stringsproj.package1;
+
+public class Stringcode2 {
+
+	public static void main(String[] args) {
+		String s1 = new String("Monisha");
+		System.out.println(s1);
+		// TODO Auto-generated method stub
+
+	}
+
+}
