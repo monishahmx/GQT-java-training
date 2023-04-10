296fe57d5ad26a27871080e8d6590b58406e2b00
Monisha <Monisha@DESKTOP-IRH1S1V>
2023-04-10 10:40:18 +0530

    commit

---
+++ src/com/gqt/stringsproj/package1/Stringcode4.java
@@ -0,0 +1,13 @@
+package com.gqt.stringsproj.package1;
+
+public class Stringcode4 {
+
+	public static void main(String[] args) {
+		char ch[] = {'M','o','n','i','s','h','a'};
+		String s1 = new String(ch);
+		System.out.println(s1);
+		// TODO Auto-generated method stub
+
+	}
+
+}
