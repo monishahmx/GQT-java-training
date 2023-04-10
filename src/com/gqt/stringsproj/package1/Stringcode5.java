3c4a6742f03945d5dd9dd751eb26854a56e2eef8
Monisha <Monisha@DESKTOP-IRH1S1V>
2023-04-10 11:44:06 +0530

    commit

---
+++ src/com/gqt/stringsproj/package1/Stringcode5.java
@@ -0,0 +1,25 @@
+package com.gqt.stringsproj.package1;
+import java.util.Scanner;
+
+public class Stringcode5 {
+
+	public static void main(String[] args) {
+		Scanner sc =new Scanner(System.in);
+		System.out.println("Enter the string:");
+		 String s1 = sc.nextLine();
+		//s1 = s1.toLowerCase();
+		//s1=s1.toUpperCase();
+		//s1 =s1.substring(5,8);
+		//s1 = s1.substring(8);
+		//int l =s1.length();
+		//char ch[]=s1.toCharArray();
+		//s1 = s1 +  "by weeknd";
+		char ch = s1.charAt(6);
+		System.out.println(ch);
+		
+		// TODO Auto-generated method stub
+
+	
+
+}
+}
