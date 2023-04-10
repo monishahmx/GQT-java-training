a0d8a632872f1ebfb527d665a73cd5a8e02dd2b5
Monisha <Monisha@DESKTOP-IRH1S1V>
2023-04-10 14:25:31 +0530

    commit

---
+++ src/Javaprogram/Labelled.java
@@ -0,0 +1,14 @@
+package Javaprogram;
+
+public class Labelled {
+	public static void main(String[] args) {
+		outer:for(int i =1;i<=3;i++) {
+			System.out.println(i);
+			inner:for(int j=1;j<=3;j++) {
+				System.out.println("WEEKND");
+				break outer;
+			}
+		}
+	}
+
+}
