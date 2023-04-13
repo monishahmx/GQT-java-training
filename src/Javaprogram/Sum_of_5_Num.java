57f63eb6a47497078b0be9bd8a1daa6e524d0a9f
Monisha <Monisha@DESKTOP-IRH1S1V>
2023-04-13 12:37:44 +0530

    commit

---
+++ src/Javaprogram/Sum_of_5_Num.java
@@ -0,0 +1,16 @@
+package Javaprogram;
+
+public class Sum_of_5_Num {
+
+	public static void main(String[] args) {
+		int sum =0;
+		for(int i =1;i<=5;i++) {
+			sum = sum+i;
+			
+		}
+		System.out.println("sum og first 5 natural numbers is"+  sum);
+		// TODO Auto-generated method stub
+
+	}
+
+}
