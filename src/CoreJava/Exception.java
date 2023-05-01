d5a479271e4be923b966541eae482104f7a52863
Monisha <Monisha@DESKTOP-IRH1S1V>
2023-05-01 13:16:43 +0530

    commit

---
+++ src/CoreJava/Exception.java
@@ -0,0 +1,18 @@
+package CoreJava;
+import java.util.Scanner;
+
+public class Exception {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the first num n1:");
+		int n1 = sc.nextInt();//syntax error
+		System.out.println("Enter the first num n2:");
+		int n2 = sc.nextInt();
+		int res = n1/n2;
+		System.out.println(res);
+		// TODO Auto-generated method stub
+
+	}
+s
+}
