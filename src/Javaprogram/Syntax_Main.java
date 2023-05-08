d619e9af388b29a4f25062bf9e7fae412325f56d
Monisha <Monisha@DESKTOP-IRH1S1V>
2023-05-08 14:57:08 +0530

    commit

---
+++ src/Javaprogram/Syntax_Main.java
@@ -0,0 +1,16 @@
+package Javaprogram;
+
+public class Syntax_Main {
+
+	 public static void main(String... args) {
+		 //public static void main(String args[]){
+	//	static public  void main(String... args) {
+		// static public  void main(String[] args) {
+		// static public  void main(String args[]) {
+		 
+		System.out.println("Hello world");
+		// TODO Auto-generated method stub
+
+	}
+
+}
