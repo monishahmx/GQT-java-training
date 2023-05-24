b6db5cb747581157cb0daa5ff79738fdb3cecefa
Monisha <Monisha@DESKTOP-IRH1S1V>
2023-05-24 06:10:34 +0530

    commit

---
+++ src/PracticeQuestions/Practice_1.java
@@ -0,0 +1,19 @@
+package PracticeQuestions;
+
+public class Practice_1 {
+
+	public static void main(String[] args) {
+		int count = 25;
+		for(int i=5;i<0;i++) {
+			for(int j=5;j<0;j++) {
+				System.out.println(count+"");
+				
+			}
+			count--;
+			
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
